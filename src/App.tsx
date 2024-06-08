3c6ec011678bc9e3856b48afe5624e5c4d81e74c
JuanesGH <jheras@2amigos.us>
2024-06-08 11:38:30 +0200

    1 step done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ function App() {
 
   return (
     <>
-      <h1>Constrol de gastos</h1>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello, Tailwind CSS with Vite and React!
+        </h1>
+      </div>
     </>
   )
 }
