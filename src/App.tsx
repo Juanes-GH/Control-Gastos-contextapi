7dec90e773f9e66566b2ec85320cba2a5723c0a4
JuanesGH <jheras@2amigos.us>
2024-06-25 18:36:20 +0200

    add a customehook to use the provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import BudgetForm from "./components/BudgetForm"
+import { useBudget } from "./hooks/useBudget"
 
 function App() {
+
+  const {state, dispatch} = useBudget();
+  console.log(state)
 
   return (
     <>
