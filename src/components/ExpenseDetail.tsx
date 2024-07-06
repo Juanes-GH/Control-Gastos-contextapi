f9cd3b22d9204910132c8091d2c9c0d76e7eba22
JuanesGH <jheras@2amigos.us>
2024-07-06 15:18:48 +0200

    add the the date

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -8,13 +8,23 @@ type ExpenseDetailProps = {
 
 export default function ExpenseDetail({expense}:ExpenseDetailProps) {
 
-  const {amount, expenseName, category, date} = expense
-  console.log(date, 'fecha')
+  const {amount, expenseName, category, date} = expense;
+
+  const formattedDate = date 
+  ? Array.isArray(date) 
+    ? new Date(date[0]!).toLocaleDateString() 
+    : new Date(date).toLocaleDateString()
+  : '';
+
+  function getExpenseCategory(category:string) {
+    return category === "Gastos Varios" ? "gastos" : category;
+  }
+
 
   return (
     <div className="flex items-center p-8">
       <div className="w-1/4">
-        <img src={`./public/icono_${category}.svg`} alt="categoryIcon" className="w-16 h-16"/>
+        <img src={`./public/icono_${getExpenseCategory(category)}.svg`} alt="categoryIcon" className="w-16 h-16"/>
       </div>
 
       <div className="w-3/4 flex flex-col">
@@ -30,14 +40,14 @@ export default function ExpenseDetail({expense}:ExpenseDetailProps) {
             </p>
             <p className="text-2xl text-blue-400 font-bold">
               Dia del gasto:{' '}
-              <span className="text-black font-normal">{"08/20/2024"}</span>
+              <span className="text-black font-normal">{formattedDate}</span>
             </p>
           </div>
           <div className="flex flex-col space-y-7">
-            <button className="w-8 h-8 hover:cursor-pointer">
+            <button className="w-8 h-8 hover:cursor-pointer hover:text-red-600">
               <TrashIcon />
             </button>
-            <button className="w-8 h-8 hover:cursor-pointer" >
+            <button className="w-8 h-8 hover:cursor-pointer hover:text-orange-400" >
               <PencilSquareIcon />
             </button>
           </div>
